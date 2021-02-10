f3ca501af62db0a90a204f95c0ca11a84755b1d4
Tommy Evans-Barton <tevansbarton@Tommys-MBP.lan>
2021-02-10 14:12:03 -0800

    Latest update

--- src/modeling.py
+++ src/modeling.py
@@ -7,10 +7,18 @@ import numpy as np
 import json
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
+from sklearn.model_selection import KFold
 from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import OneHotEncoder
-from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import Binarizer
+from sklearn.preprocessing import FunctionTransformer
 from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import Ridge
+from sklearn.linear_model import Lasso
 from sklearn.pipeline import Pipeline
+from sklearn.metrics import r2_score
+from sklearn.metrics import mean_squared_error
+import scipy.stats
 
 TOP_PATH = os.environ['PWD']
+
